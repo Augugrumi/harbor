ff684fc2b1594b71f4678e1b5cb0afd3a384c90e
Davide Polonio <poloniodavide@gmail.com>
2018-08-01 14:35:35 +0200

    Triggering docker build

--- src/main/java/routes/vnf/LauncherRoute.java
+++ src/main/java/routes/vnf/LauncherRoute.java
@@ -43,7 +43,6 @@ public class LauncherRoute implements Route {
             LOG.info(item.getMetadata().getName());
         }
 
-
         JSONObject res = new JSONObject();
         res.put("result", "ok");
 
