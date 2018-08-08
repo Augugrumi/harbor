f538915e63801fa66537370b4c3b4958ace2bb9a
Davide Polonio <poloniodavide@gmail.com>
2018-08-08 09:52:13 +0200

    Add UpdateVnfRoute

---
+++ src/main/java/routes/vnf/UpdateVnfRoute.java
@@ -0,0 +1,39 @@
+package routes.vnf;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import util.ConfigManager;
+
+public class UpdateVnfRoute implements Route {
+
+    final private static Logger LOG = ConfigManager.getConfig().getApplicationLogger(UpdateVnfRoute.class);
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+
+        LOG.debug("DeleteVnfRoute called");
+
+        // Update = Deletion + Creation
+        Route deletion = new DeleteVnfRoute();
+        Route creation = new UpdateVnfRoute();
+
+        JSONObject toSendBack = new JSONObject();
+
+        JSONObject reply = (JSONObject) deletion.handle(request, response);
+        if ("ok".equals(reply.getString("result"))) {
+            reply = (JSONObject) creation.handle(request, response);
+            if ("ok".equals(reply.getString("result"))) {
+                toSendBack.put("result", "ok");
+            } else {
+                toSendBack = reply;
+            }
+        } else {
+            toSendBack = reply;
+        }
+
+        return toSendBack;
+    }
+}
