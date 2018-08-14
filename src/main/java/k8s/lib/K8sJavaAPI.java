2010156f4f03a23207d03d9ccbcf28a7dd53efd7
Davide Polonio <poloniodavide@gmail.com>
2018-08-14 13:22:23 +0200

    Fix NoImplementedException with RuntimeException

--- src/main/java/k8s/lib/K8sJavaAPI.java
+++ src/main/java/k8s/lib/K8sJavaAPI.java
@@ -3,14 +3,13 @@ package k8s.lib;
 import k8s.K8sAPI;
 import k8s.K8sResultConverter;
 import k8s.exceptions.K8sException;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.net.URL;
 
 public class K8sJavaAPI implements K8sAPI {
 
     public K8sJavaAPI() {
-        throw new NotImplementedException();
+        throw new RuntimeException();
     }
 
     @Override
