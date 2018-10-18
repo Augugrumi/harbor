0e7cf51e93a2546e1b2585c6683fde80561125c9
Davide Polonio <poloniodavide@gmail.com>
2018-10-18 12:57:56 +0200

    Add errors messages for POST requests

--- src/main/java/org/augugrumi/harbor/routes/util/Errors.java
+++ src/main/java/org/augugrumi/harbor/routes/util/Errors.java
@@ -2,7 +2,8 @@ package org.augugrumi.harbor.routes.util;
 
 public interface Errors {
     String NO_SUCH_ELEMENT = "The requested element was not found";
-    String NO_VNF_FOUND = "The following VNF were not found: ";
     String DB_ADD = "Impossible to add the object in the database";
     String DB_REMOVE = "Impossible to remove the object from the database";
+    String ROULETTE_UPDATE_FAILURE = "Impossible to update Roulette entry";
+    String ROULETTE_EMPTY_REPLY = "Roulette replied with an empty message";
 }
